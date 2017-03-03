8b66cb21470e7ad8b6d5e766cfaefa0cfd93952f
wjase <wjase@yahoo.com.au>
2017-03-04 07:52:02 +1100

    Improving error reporting and converter registration

--- src/main/java/com/cybernostics/jsp2thymeleaf/api/expressions/function/FunctionConverterSource.java
+++ src/main/java/com/cybernostics/jsp2thymeleaf/api/expressions/function/FunctionConverterSource.java
@@ -6,12 +6,16 @@
 package com.cybernostics.jsp2thymeleaf.api.expressions.function;
 
 import com.cybernostics.jsp2thymeleaf.api.expressions.ExpressionVisitor;
+import static com.cybernostics.jsp2thymeleaf.api.expressions.function.DefaultFunctionExpressionConverter.convertsMethodCall;
 import com.cybernostics.jsp2thymeleaf.api.util.PrefixedName;
 import static com.cybernostics.jsp2thymeleaf.api.util.PrefixedName.prefixedNameFor;
+import com.cybernostics.jsp2thymeleaf.api.util.SimpleStringTemplateProcessor;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 import org.apache.commons.el.Expression;
 
 /**
@@ -32,6 +36,26 @@ public class FunctionConverterSource
     public FunctionConverterSource()
     {
 
+    }
+
+    public static FunctionConverterSource forUri(String forUri)
+    {
+        return new FunctionConverterSource(forUri);
+    }
+
+    public FunctionConverterSource convertsMethodsUsingFormat(String methodFormat, String... inputFunctions)
+    {
+        Arrays.stream(inputFunctions)
+                .map(method -> convertsMethodCall(method).toMethodCall(format(methodFormat, method)))
+                .forEach(converter -> add(converter));
+        return this;
+    }
+
+    private String format(String methodFormat, String method)
+    {
+        Map<String, String> values = new TreeMap<>();
+        values.put("method", method);
+        return SimpleStringTemplateProcessor.generate(methodFormat, values);
     }
 
     public FunctionConverterSource(String forUri, ExpressionVisitor... converters)
